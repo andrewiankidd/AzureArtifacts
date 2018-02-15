e93365062bd6edefdb2c1299af98fcce5df6ab5c
Andrew Kidd <contact@andrewiankidd.co.uk>
2018-02-15 16:26:08 +0000

    Update sqlserver.ps1

--- sqlserver.ps1
+++ sqlserver.ps1
@@ -20,21 +20,21 @@ if (!(Test-Path("$env:temp\SQLFULL_x64_ENU_Core.box")))
 {
 	write-output "Downloading SQLServer Core..."
 	$downloadURL = "https://download.microsoft.com/download/4/C/7/4C7D40B9-BCF8-4F8A-9E76-06E9B92FE5AE/ENU/x64/SQLFULL_x64_ENU_Core.box"
-	(New-Object System.Net.WebClient).DownloadString($downloadURL, "$env:temp\SQLFULL_x64_ENU_Core.box")
+	$webClient.DownloadFile($downloadURL, "$env:temp\SQLFULL_x64_ENU_Core.box")
 }
 
 if (!(Test-Path("$env:temp\SQLFULL_x64_ENU_Lang.box")))
 {
 	write-output "Downloading SQLServer Language Files..."
 	$downloadURL = "https://download.microsoft.com/download/4/C/7/4C7D40B9-BCF8-4F8A-9E76-06E9B92FE5AE/ENU/x64/SQLFULL_x64_ENU_Lang.box"
-	(New-Object System.Net.WebClient).DownloadString($downloadURL, "$env:temp\SQLFULL_x64_ENU_Lang.box")
+	$webClient.DownloadFile($downloadURL, "$env:temp\SQLFULL_x64_ENU_Lang.box")
 }
 
 if (!(Test-Path("$env:temp\SQLFULL_x64_ENU_Install.exe")))
 {
 	write-output "Downloading SQLServer Installer..."
 	$downloadURL = "https://download.microsoft.com/download/4/C/7/4C7D40B9-BCF8-4F8A-9E76-06E9B92FE5AE/ENU/x64/SQLFULL_x64_ENU_Install.exe"
-	(New-Object System.Net.WebClient).DownloadString($downloadURL, "$env:temp\SQLFULL_x64_ENU_Install.exe")
+	$webClient.DownloadFile($downloadURL, "$env:temp\SQLFULL_x64_ENU_Install.exe")
 }
 
 Write-Output "Installing SQLServer";
