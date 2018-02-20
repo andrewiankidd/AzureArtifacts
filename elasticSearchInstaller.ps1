d03e0e2a7c246238f576ad2fbc8347e58caf6e3b
Andrew Kidd <contact@andrewiankidd.co.uk>
2018-02-20 14:05:36 +0000

    Update elasticSearchInstaller.ps1

--- elasticSearchInstaller.ps1
+++ elasticSearchInstaller.ps1
@@ -34,23 +34,23 @@ else{
 }
 
 # Download Java http://www.weirdwindowsfixes.com/2017/05/powershell-download-and-install-java.html
-$javax64install = ((Invoke-WebRequest –Uri 'https://www.java.com/en/download/manual.jsp').Links | Where-Object { $_.innerHTML -eq "Windows Offline (64-bit)" }).href
-$javax64 = "$env:temp\java.exe"
-If !((Test-Path($javax64)))
+$javax64install = "http://javadl.oracle.com/webapps/download/AutoDL?BundleId=230542_2f38c3b165be4555a1fa6e98c45e0808";
+$javax64 = "$env:temp\java.exe";
+If (!(Test-Path($javax64)))
 {
     Write-Output "Downloading $javax64install"
-    $start_time = Get-Date
-    (New-Object System.Net.WebClient).DownloadFile($javax64install, $javax64)
-    Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+    $start_time = Get-Date;
+    (New-Object System.Net.WebClient).DownloadFile($javax64install, $javax64);
+    Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)";
     
     # Install Java
     $javax64install = Start-Process -FilePath $javax64 -ArgumentList "/s INSTALL_SILENT=1 STATIC=0 AUTO_UPDATE=0 WEB_JAVA=1 WEB_JAVA_SECURITY_LEVEL=H WEB_ANALYTICS=0 EULA=0 REBOOT=0 NOSTARTMENU=0 SPONSORS=0 /L $Logfile" -Wait -Verbose -PassThru
 	Start-Sleep -s 35
 	if ($javax64install.ExitCode -eq 0) {
-		Write-Output "Successfully Installed Java RE X64"
+		Write-Output "Successfully Installed Java RE X64";
 	}
 	else {
-		Write-Error "Java 64 bit installer exited with exit code $($javax64install.ExitCode)"
+		Write-Error "Java 64 bit installer exited with exit code $($javax64install.ExitCode)";
 	}
 }
 
