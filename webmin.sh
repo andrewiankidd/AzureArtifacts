9b4c8b13d7e9197500838539bef5ee2f5676cd81
Andrew Kidd <contact@andrewiankidd.co.uk>
2018-04-26 10:41:41 +0100

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
 wget http://prdownloads.sourceforge.net/webadmin/webmin_1.881_all.deb
-sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+sudo apt-get install -y perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 sudo dpkg --install webmin_1.881_all.deb
