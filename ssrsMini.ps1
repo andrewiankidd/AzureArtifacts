a62d61a29ea97bd3247b11f9b22d86b172e84d02
Andrew Kidd <contact@andrewiankidd.co.uk>
2019-01-28 13:48:57 +0000

    Update ssrsMini.ps1

--- ssrsMini.ps1
+++ ssrsMini.ps1
@@ -166,8 +166,14 @@ if ($reportUser -ne $null)
     }
     
     # Connect to (localhost) SSRS service
+    $ssrs = null;
+    $start = Get-Date
+    # Try for FIVE minutes
     Write-Output "New-WebServiceProxy -Uri `"http://localhost/ReportServer/ReportService2010.asmx?wsdl`" -Credential (New-Object System.Management.Automation.PSCredential (`"$adminUser`", (ConvertTo-SecureString `"$adminPassword`" -AsPlainText -Force)))";
-    $ssrs = New-WebServiceProxy -Uri "http://localhost/ReportServer/ReportService2010.asmx?wsdl" -Credential (New-Object System.Management.Automation.PSCredential ("$adminUser", (ConvertTo-SecureString "$adminPassword" -AsPlainText -Force)));
+    while ((!$ssrs) -and ((New-TimeSpan -Start $start -End (Get-Date)).TotalSeconds -lt 300)) {
+        $ssrs = New-WebServiceProxy -Uri "http://localhost/ReportServer/ReportService2010.asmx?wsdl" -Credential (New-Object System.Management.Automation.PSCredential ("$adminUser", (ConvertTo-SecureString "$adminPassword" -AsPlainText -Force))) -ErrorAction SilentlyContinue;
+    }
+    
     $namespace = $ssrs.GetType().Namespace;
     $changesMade = $false;
     $policies = $null;
