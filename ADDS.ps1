a2fc7a1ba433ea6eff7288a6a468ba3f23abe94a
Andrew Kidd <contact@andrewiankidd.co.uk>
2019-05-15 21:37:50 +0100

    Update ADDS.ps1

--- ADDS.ps1
+++ ADDS.ps1
@@ -36,6 +36,7 @@ if (!(Test-Path "F:")){
 
 # Create PSCredentials object
 $credStore = New-Object System.Management.Automation.PSCredential("$domainName\$adminUsername", $securePassword);
+$localCredStore = New-Object System.Management.Automation.PSCredential("$adminUsername", $securePassword);
 
 # Add the missing windows features
 Write-Output "Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools";
@@ -96,7 +97,7 @@ if ($deployIndex -eq 1) {
 
 	# Joining Domain/Forest
 	Write-Output "Install-ADDSDomainController";
-	Start-Process 'powershell.exe' -Credential $credStore -ArgumentList "Install-ADDSDomainController -CreateDnsDelegation:$false -DatabasePath 'F:\NTDS' -DomainName '$domainname' -NoGlobalCatalog:$false -InstallDns:$false -LogPath 'F:\NTDS' -SiteName 'Default-First-Site-Name' -SysvolPath 'F:\SYSVOL' -NoRebootOnCompletion:$true -Credential $credStore -SafeModeAdministratorPassword $securePassword -Force:$true"
+	Start-Process 'powershell.exe' -Credential $localCredStore -ArgumentList "Install-ADDSDomainController -CreateDnsDelegation:$false -DatabasePath 'F:\NTDS' -DomainName '$domainname' -NoGlobalCatalog:$false -InstallDns:$false -LogPath 'F:\NTDS' -SiteName 'Default-First-Site-Name' -SysvolPath 'F:\SYSVOL' -NoRebootOnCompletion:$true -Credential $credStore -SafeModeAdministratorPassword $securePassword -Force:$true"
 	Restart-Computer -Force;
 }
 
